424de15b23d8960a795f939e8b81b14aef43e201
normandparatte <normand.paratte@gmail.com>
2018-05-05 11:58:03 +0200

    Préparation du menu et appelle des bonnes fonctions + boucle tant que l'utilisateur n'a pas choisi de quitter l'application

--- src/main/java/ch/hearc/ig/st/adfgvx/metier/Adfgvx.java
+++ src/main/java/ch/hearc/ig/st/adfgvx/metier/Adfgvx.java
@@ -1,5 +1,11 @@
 package ch.hearc.ig.st.adfgvx.metier;
 
 public class Adfgvx {
+  public static String chiffrerPhrase(String phraseADechiffrer){
+    return "";
+  }
 
+  public static String dechiffrerPhrase(String phraseADechiffrer){
+    return "";
+  }
 }
--- src/main/java/ch/hearc/ig/st/adfgvx/vue/Ihm.java
+++ src/main/java/ch/hearc/ig/st/adfgvx/vue/Ihm.java
@@ -1,35 +1,44 @@
 package ch.hearc.ig.st.adfgvx.vue;
 
+import ch.hearc.ig.st.adfgvx.metier.Adfgvx;
 import java.util.Scanner;
 
 public class Ihm {
   Scanner sc = new Scanner(System.in);
   String p1;
-  String p2;
+  String phrase;
 
   public void menu() {
-    System.out.println("\n\n=====================================");
-    System.out.println("======== Chiffre ADFGVX - Menu ======");
-    System.out.println("=====================================\n");
+    while (true) {
+      System.out.println("\n\n=====================================");
+      System.out.println("======== Chiffre ADFGVX - Menu ======");
+      System.out.println("=====================================\n");
 
-    System.out.println("1. Chiffrer une phrase");
-    System.out.println("2. Déchiffrer une phrase");
-    System.out.println("0. Quitter l'application\n");
+      System.out.println("1. Chiffrer une phrase");
+      System.out.println("2. Déchiffrer une phrase");
+      System.out.println("0. Quitter l'application\n");
 
-    System.out.print("Votre sélection : ");
-    p1 = sc.nextLine();
-    System.out.println("");
+      System.out.print("Votre sélection : ");
+      p1 = sc.nextLine();
+      System.out.println("");
 
-    switch (p1.charAt(0)){
-      case '1':
-        System.out.println("Choix 1");
-        break;
-      case '2':
-        System.out.println("Choix 2");
-        break;
-      case '0':
-        System.exit(0);
-        break;
+      switch (p1.charAt(0)) {
+        case '1':
+          System.out.println("Veuillez saisir la phrase à chiffrer :");
+          phrase = sc.nextLine();
+          phrase = Adfgvx.chiffrerPhrase(phrase);
+          System.out.println("Votre phrase chiffrée est : "+phrase);
+          break;
+        case '2':
+          System.out.println("Veuillez saisir la phrase à déchiffrer :");
+          phrase = sc.nextLine();
+          phrase = Adfgvx.dechiffrerPhrase(phrase);
+          System.out.println("Votre phrase déchiffrée est : "+phrase);
+          break;
+        case '0':
+          System.exit(0);
+          break;
+      }
     }
   }
 }
